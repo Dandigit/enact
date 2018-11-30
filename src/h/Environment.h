22ff422b04a8365bcdf7964ca8033ef6115bc2a7
Dandigit <37099129+Dandigit@users.noreply.github.com>
2018-11-30 14:04:12 +1000

    Update Environment.h

--- src/h/Environment.h
+++ src/h/Environment.h
@@ -1,8 +1,21 @@
 #ifndef MATILDA_ENVIRONMENT_H
 #define MATILDA_ENVIRONMENT_H
 
+#include "Value.h"
+
+#include <unordered_map>
+
 class Environment {
-    Environment *enclosing;
+private:
+    Environment *m_enclosing;
+    std::unordered_map<std::string, Value> m_variables;
+public:
+    Environment(Environment* enclosing);
+    Value* find(const std::string &name);
+    bool contains(const std::string &name);
+    void set(const std::string &name, Value value);
+    void create(const std::string &name, Value value);
 };
+
 
 #endif //MATILDA_ENVIRONMENT_H
